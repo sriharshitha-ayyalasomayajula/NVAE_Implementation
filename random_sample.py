815c7055b682aee7e4549efe4fff2e521ce016ff
Sriharshitha_Ayyalasomayajula <sriharshitha.ayyalasomayajula@gmail.com>
2023-09-19 16:29:24 -0700

    random sample

---
+++ random_sample.py
@@ -0,0 +1,30 @@
+import torch
+
+from nvae.utils import add_sn
+from nvae.vae_celeba import NVAE
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    device = "cpu"
+    model = NVAE(z_dim=32, img_dim=(64, 64))
+    model.apply(add_sn)
+    model.to(device)
+
+    # model.load_state_dict(torch.load("checkpoints/ae_ckpt_169_0.689621.pth", map_location=device), strict=False)
+
+    model.eval()
+
+    with torch.no_grad():
+        z = torch.randn((24,32,1,1)).to(device)
+        gen_imgs, _ = model.decoder(z)
+        print(gen_imgs.shape)
+        # gen_imgs = gen_imgs.permute(0, 2, 3, 1)
+
+        # for gen_img in gen_imgs:
+        #     gen_img = gen_img.cpu().numpy() * 255
+        #     gen_img = gen_img.astype(np.uint8)
+
+        #     plt.imshow(gen_img)
+        #     # plt.savefig(f"output/ae_ckpt_%d_%.6f.png" % (9, 0.981127))
+        #     plt.show()
